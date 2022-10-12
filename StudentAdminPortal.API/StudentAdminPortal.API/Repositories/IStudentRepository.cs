afd0bb101377a815a60a7f765918ea814e01ea62
Ashish4883 <aashishmishra0016@gmail.com>
2022-10-12 22:42:24 +0530

    Second Commit

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/IStudentRepository.cs
@@ -0,0 +1,16 @@
+﻿using StudentAdminPortal.API.DataModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Repositories
+{
+    public interface IStudentRepository
+    { 
+        //List<Student> GetStudents();
+
+        Task<List<Student>> GetStudentsAsync();
+
+    }
+}
