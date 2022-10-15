2bf21e853779f7e041ee54377fefaee72716bb79
Ashish4883 <aashishmishra0016@gmail.com>
2022-10-15 21:37:40 +0530

    Upload_Image Feature Added

---
+++ StudentAdminPortal.API/StudentAdminPortal.API/Repositories/IImageRepository.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.Repositories
+{
+    public interface IImageRepository
+    {
+       Task<String> Upload(IFormFile file, string fileName);
+    }
+}
